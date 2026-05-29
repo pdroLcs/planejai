b81b56ed8ed3988c784dea98bf42d9491008ad3b
Pedro Lucas <plfm3k@gmail.com>
2026-05-29 13:18:49 -0400

    feat: adiciona navegação no formulário

--- src/components/features/simulation/Form.tsx
+++ src/components/features/simulation/Form.tsx
@@ -1,16 +1,30 @@
-import { PiggyBank } from "lucide-react";
 import { FormStep } from "./FormStep";
 import { StepProgress } from "./Progress";
 import { simulationFormSteps } from "../../../data/Simulation";
+import { useState } from "react";
 
 export function SimulationForm() {
 
-  const currestStep = simulationFormSteps[0];
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+  const totalSteps = simulationFormSteps.length;
+  const currestStep = simulationFormSteps[currentStepIndex];
+
+  const handleNextStep = () => {
+    if (currentStepIndex + 1 > totalSteps - 1) return;
+
+    setCurrentStepIndex(prev => prev + 1);
+  }
+
+  const handlePreviousStep = () => {
+    if (currentStepIndex === 0) return;
+
+    setCurrentStepIndex(prev => prev - 1);
+  }
 
   return (
     <>
-      <StepProgress currestStep={1} totalSteps={6}/>
-      <FormStep key={currestStep.id} {...currestStep}/>
+      <StepProgress currestStep={currentStepIndex + 1} totalSteps={totalSteps}/>
+      <FormStep key={currestStep.id} {...currestStep} onBack={handlePreviousStep} onNext={handleNextStep} hideBackButton={currentStepIndex === 0}/>
     </>
   );
 }
