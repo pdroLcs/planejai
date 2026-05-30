502ddbecb1790e309a2894f7b518a6b27cfa2eb5
Pedro Lucas <plfm3k@gmail.com>
2026-05-29 23:58:12 -0400

    feat: adiciona esqueleto de carregamento e componente de erro

--- src/components/features/simulationResults/AiInsightCard.tsx
+++ src/components/features/simulationResults/AiInsightCard.tsx
@@ -1,16 +1,41 @@
+import Skeleton from "react-loading-skeleton";
 import { useInsight } from "../../../hooks/userInsight"
+import { Content } from "../insights/Content";
+import { Error } from "../insights/Error";
+
+import 'react-loading-skeleton/dist/skeleton.css';
 
 interface AiInsightCardProps {
   simulationId: string
 }
 
 export function AiInsightCard({simulationId}: AiInsightCardProps) {
-  const {insight} = useInsight(simulationId);
+  const {insight, isLoading, error, fetchInsight} = useInsight(simulationId);
   console.log(insight);
 
   return (
     <div className="bg-card order-2 rounded-2xl p-6 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)] lg:order-1 lg:col-span-2">
-      Painel de Insights
+      <div className="mb-3 flex items-center gap-1.5">
+        <span>✨</span>
+        <span className="text-primary text-xs font-semibold tracking-widest uppercase">
+          Insight Financeiro Personalizado
+        </span>
+      </div>
+
+      {isLoading && (
+        <div className="flex">
+          <Skeleton 
+            count={10.5}
+            baseColor="var(--color-skeleton-base)"
+            highlightColor="var(--color-skeleton-highlight)"
+            className="mb-3 flex rounded-lg"
+            containerClassName="flex-1"
+            inline
+          />
+        </div>
+      )}
+      {!isLoading && error && <Error simulationId={simulationId} message={error} onRetry={() => fetchInsight(simulationId)}/>}
+      {!isLoading && insight && !error && <Content/>}
     </div>
   )
 }
